956b6b10d71e539d28b94c9e1baaa75c8b877e4a
mraabo <magnusraabo@gmail.com>
2026-08-26 09:39:00 +0200

    make org-download save images in subfolder named after nearest org-download-heaing-lvl in snakescript

--- init.el
+++ init.el
@@ -6,11 +6,38 @@
  '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
  '(backup-directory-alist '((".*" . "~/.emacs.d/backups/")))
  '(custom-safe-themes
-   '("e900ae738225380abd1edc0c76535a12b8a6669c7a3180431ba0157a47bbf75e" "8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098" "014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69" "fd22a3aac273624858a4184079b7134fb4e97104d1627cb2b488821be765ff17" "0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1" default))
+   '("720838034f1dd3b3da66f6bd4d053ee67c93a747b219d1c546c41c4e425daf93" "e900ae738225380abd1edc0c76535a12b8a6669c7a3180431ba0157a47bbf75e" "8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098" "014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69" "fd22a3aac273624858a4184079b7134fb4e97104d1627cb2b488821be765ff17" "0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1" default))
  '(package-selected-packages
    '(elfeed listen ess catppuccin-theme org-journal all-the-icons-ivy-rich dape dap-dlv-go go-mode go org-roam-ui python-mode fontawesome abc-mode abs-mode quelpa-use-package elfeed-org ox-hugo exec-path-from-shell lsp-ivy lsp-haskell dap-haskell dap-mode helm-lsp lsp-ui haskell-mode quelpa gamify which-key projectile all-the-icons helpful counsel ivy doom-modeline helm lsp-mode svg-tag-mode olivetti org-download magit org-roam org-fragtog org-appear org-superstar jinx pdf-tools doom-themes auctex))
  '(safe-local-variable-values
-   '((eval setq-local org-download-image-dir
+   '((eval progn
+	   (org-hugo-auto-export-mode)
+	   (add-hook 'before-save-hook
+		     (lambda nil
+		       (let*
+			   ((source-root
+			     (locate-dominating-file
+			      (buffer-file-name)
+			      ".dir-locals.el"))
+			    (rel-path
+			     (when source-root
+			       (file-relative-name
+				(buffer-file-name)
+				source-root)))
+			    (rel-dir
+			     (when rel-path
+			       (file-name-directory rel-path))))
+			 (when source-root
+			   (setq-local org-hugo-section
+				       (if
+					   (or
+					    (null rel-dir)
+					    (string= rel-dir "./")
+					    (string= rel-dir "."))
+					   nil
+					 (directory-file-name rel-dir))))))
+		     nil t))
+     (eval setq-local org-download-image-dir
 	   (concat "~/personal_website/static/htb_writeups/"
 		   (file-name-base
 		    (buffer-file-name))))
@@ -512,7 +539,9 @@
 		 (clean-base (replace-regexp-in-string
 			      " " "_"
 			      (replace-regexp-in-string ":" "-" (downcase base)))))
-	    (concat clean-base "_" timestamp "." ext))))
+	    (concat clean-base "_" timestamp "." ext)))
+	;; Stop adding #downloaded properties above pasted image link
+	org-download-annotate-function (lambda (_link) ""))
 	;; Images are nested in subfolders named after the nearest
 	;; org-download-heading-lvl header.
 	;; This converts the subfolder name to snakescript to avoid spaces in foldernames
